f2670de57c466a70b21d7119971debc877526e08
josewebfreelance <jose.webfreelance@gmail.com>
2018-11-23 07:12:04 -0600

    Cambios en properties

--- spring-boot-web-jsp/src/main/java/com/example/springbootwebjsp/IndexController.java
+++ spring-boot-web-jsp/src/main/java/com/example/springbootwebjsp/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springbootwebjsp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    @Value("${application.controller.titulo}")
+    private String titulo;
+
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("titulo", "Spring boot hola mundo");
+        model.addAttribute("titulo", this.titulo);
         return "index";
     }
 }
